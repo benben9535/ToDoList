9d0416032ae0e1c13225ac95f462f46a7780934d
benben <benben9535@gmail.com>
2021-08-12 16:50:32 +0800

    feat: 基本功能完成
    - 新增、修改、刪除項目
    - 置頂項目
    - 增加過渡動畫

--- main.js
+++ main.js
@@ -1,15 +1,76 @@
 const { createApp, ref } = Vue;
+
 const vm = createApp({
   data() {
     return {
       time: {},
-      actived: 'all'
+      addTaskContent: '',
+      editTaskContent: '',
+      editID: 0,
+      actived: 'all',
+      list:[],
     }
   },
-  mounted() {
+  computed: {
+    showList() {
+      let list = []
+      if (this.actived === 'all') {
+        list = this.list.filter( item => {
+          return true
+        }).sort((itemA, itemB) => {
+          if ((itemA.pin && itemB.pin)) {
+            return itemA.id - itemB.id
+          } else if ((itemA.pin)) {
+            return -1
+          } else if ((itemB.pin)) {
+            return 1
+          } else {
+            return itemA.id - itemB.id
+          }
+        })
+      } else if (this.actived === 'todo') {
+        list = this.list.filter( item => {
+          return item.done === false
+        }).sort((itemA, itemB) => {
+          if ((itemA.pin && itemB.pin)) {
+            return itemA.id - itemB.id
+          } else if ((itemA.pin)) {
+            return -1
+          } else if ((itemB.pin)) {
+            return 1
+          } else {
+            return itemA.id - itemB.id
+          }
+        })
+      } else if (this.actived === 'done') {
+        list = this.list.filter( item => {
+          return item.done === true
+        }).sort((itemA, itemB) => {
+          if ((itemA.pin && itemB.pin)) {
+            return itemA.id - itemB.id
+          } else if ((itemA.pin)) {
+            return -1
+          } else if ((itemB.pin)) {
+            return 1
+          } else {
+            return itemA.id - itemB.id
+          }
+        })
+      }
+      return list
+    }
+  },
+  created() {
     this.updateDate()
+    this.list = JSON.parse(localStorage.getItem('toDoList')) || []
+    window.addEventListener('beforeunload', () => {
+      localStorage.setItem('toDoList', JSON.stringify(this.list))
+    });
   },
   methods: {
+    /**
+     * 更新日期/時間
+     */
     updateDate() {
       const now = new Date()
       const monthArr = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.', 'Jul.', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.']
@@ -20,7 +81,54 @@ const vm = createApp({
         hour: now.getHours() < 10 ? '0' + now.getHours() : now.getHours(),
         minute: now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()
       }
-      setInterval(this.updateDate, 1000 * 60)
+      setInterval(this.updateDate, 1000)
+    },
+    /**
+     * 新增項目
+     */
+    addTask() {
+      if (!this.addTaskContent) return
+      this.list.push({
+        id: +new Date(),
+        content: this.addTaskContent,
+        done: false,
+        pin: false
+      })
+      this.addTaskContent = ''
+    },
+    /**
+     * 編輯項目
+     * 
+     * @param {Number} id 項目編號
+     * @param {String} content 項目內容
+     */
+     editTask(id, content) {
+      this.editID = id
+      this.editTaskContent = content
+      this.$nextTick(() => {
+        document.getElementById(`${id}`).focus()
+      })
+    },
+    /**
+     * 完成編輯
+     * 
+     * @param {Object} item 項目
+     */
+    finishEditing(item) {
+      if (this.editTaskContent) {
+        item.content = this.editTaskContent
+      }
+      this.editID = 0
+      this.editTaskContent = ''
+    },
+    /**
+     * 刪除項目
+     * 
+     * @param {Number} id 項目編號
+     */
+    deleteTask(id) {
+      const index = this.list.findIndex(item => { return item.id === id })
+      this.list.splice(index, 1)
     }
   }
 });
